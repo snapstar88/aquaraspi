fae97786b480e2fe73117cdfe183d51c5229e28b
fred <fred@SERVER>
2016-10-14 23:21:15 +0200

    ok

--- Main/Main/Main.py
+++ Main/Main/Main.py
@@ -1,66 +1,47 @@
-import serial
+
+
 import time
 import math
 
+import serial
 s = serial.Serial('/dev/ttyUSB0', 9600) # Namen ggf. anpassen
 s.isOpen()
 time.sleep(5) # der Arduino resettet nach einer Seriellen Verbindung, daher muss kurz gewartet werden
-
 s.write("test")
 
 
 
 try:
     while True:
-        
+
         response = s.readline()
         #response = "47_22_22"
-        
-           
-        
+
+
+
         responselist = response.split("_");
-        
-        
+
+
         humi = int(responselist[0]);
-        ihumi = int(humi);
-        rtemp = responselist[1];
-        irtemp = int(rtemp);
-        wtemp = responselist[2];
-        iwtemp = int(wtemp);
+        rtemp = int(responselist[1]);
+        wtemp = int(responselist[2]);
         
 
-        if ihumi >= 90:
-                    print("ACHTUNG!!!! Zimmer zu feucht! Feuchtigkeit = " + humi)
-        else:
-                    print("Zimmer OK. Feuchtigkeit = " + ihumi)
 
-        if irtemp >= 23:
-                    print("ACHTUNG!!!! Zimmer zu warm. Temperatur = " + irtemp)
+        if humi >= 90:
+                    print("ACHTUNG!!!! Zimmer zu feucht! Feuchtigkeit = ",humi)
         else:
-                    print("Zimmer OK. Temperatur = " + irtemp)
-        if iwtemp >= 25:
-                    print("AUCHTUNG!!!! Wasser zu warm. Temperatur = " + iwtemp)
+                    print("Zimmer OK. Feuchtigkeit = ", humi)
+
+        if rtemp >= 23:
+                    print("ACHTUNG!!!! Zimmer zu warm. Temperatur = ", rtemp)
         else:
-                    print("Wasser OK. Temperatur = " + iwtemp)
+                    print("Zimmer OK. Temperatur = ", rtemp)
+        if wtemp >= 25:
+                    print("AUCHTUNG!!!! Wasser zu warm. Temperatur = ", wtemp)
+        else:
+                    print("Wasser OK. Temperatur = ", wtemp)
 
 except KeyboardInterrupt:
-        #print("nix")
+        print("nix")
         s.close()
-        
-      
-        
-               
-
-
-         
-        
-
-    
-        
-   
-
-
-
-
-
-
