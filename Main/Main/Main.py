31519166939abd370c2ab8c35670ec66f1291143
fred <fred@SERVER>
2016-10-14 22:10:11 +0200

    geht

--- Main/Main/Main.py
+++ Main/Main/Main.py
@@ -1,20 +1,20 @@
-import serial
+#import serial
 import time
 import math
 
-s = serial.Serial('/dev/ttyUSB0', 9600) # Namen ggf. anpassen
-s.isOpen()
-time.sleep(5) # der Arduino resettet nach einer Seriellen Verbindung, daher muss kurz gewartet werden
+#s = serial.Serial('/dev/ttyUSB0', 9600) # Namen ggf. anpassen
+#s.isOpen()
+#time.sleep(5) # der Arduino resettet nach einer Seriellen Verbindung, daher muss kurz gewartet werden
 
-s.write("test")
+#s.write("test")
 
 
 
 try:
     while True:
         
-        response = s.readline()
-        #response = "91_23_26"
+        #response = s.readline()
+        response = "47_22_22"
         
            
         
@@ -22,31 +22,29 @@ try:
         
         humi = responselist[0];
         ihumi = int(humi);
-        humi = round(ihumi)
         rtemp = responselist[1];
         irtemp = int(rtemp);
-        rtemp = round(irtemp)
         wtemp = responselist[2];
         iwtemp = int(wtemp);
-        wtemp = round(iwtemp);
+        
 
-        if humi >= 90:
+        if ihumi >= 90:
                     print("Zimmer zu feucht")
         else:
                     print("ok")
 
-        if rtemp >= 23:
+        if irtemp >= 23:
                     print("Zimmer zu warm")
         else:
                     print("ok")
-        if wtemp >= 25:
+        if iwtemp >= 25:
                     print("Wasser zu warm")
         else:
                     print("ok")
 
 except KeyboardInterrupt:
-        #print("nix")
-        s.close()
+        print("nix")
+        #s.close()
         
       
         
