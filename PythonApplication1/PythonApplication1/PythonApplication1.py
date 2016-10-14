033b9868025cd8d43488d66ecebaaa3b3063c216
fred <fred@SERVER>
2016-10-14 17:40:37 +0200

    firt steps

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -1,4 +1,17 @@
+import serial
+import time
+
+s = serial.Serial('/dev/ttyUSB0', 9600) # Namen ggf. anpassen
+s.isOpen()
+time.sleep(5) # der Arduino resettet nach einer Seriellen Verbindung, daher muss kurz gewartet werden
+
+s.write("test")
+try:
+    while True:
+        response = s.readline()
+        print(response)
+except KeyboardInterrupt:
+    s.close()
 
 
-print = "test"
-testj 23e234 test.
+
