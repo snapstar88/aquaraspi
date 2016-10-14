4caaf33daf7f6e6b5e892286109e9c5863538045
fred <fred@SERVER>
2016-10-15 00:22:09 +0200

    fin

--- Main/Main/Main.py
+++ Main/Main/Main.py
@@ -1,47 +1,47 @@
-
-
+import serial
 import time
 import math
 
-import serial
 s = serial.Serial('/dev/ttyUSB0', 9600) # Namen ggf. anpassen
 s.isOpen()
 time.sleep(5) # der Arduino resettet nach einer Seriellen Verbindung, daher muss kurz gewartet werden
+
 s.write("test")
 
 
 
 try:
     while True:
-        time.sleep(1);
+        timte.sleep(2);
         response = s.readline()
-        #response = "47_40_40"
-
-
-
+        #response = "47_22_22"
+        
+           
+        
         responselist = response.split("_");
-
-
-        humi = int(responselist[0]);
-        rtemp = int(responselist[1]);
-        wtemp = int(responselist[2]);
+        
+        humi = responselist[0];
+        ihumi = int(humi);
+        rtemp = responselist[1];
+        irtemp = int(rtemp);
+        wtemp = responselist[2];
+        iwtemp = int(wtemp);
         
 
+        if ihumi >= 90:
+                    print("Zimmer zu feucht")
+        else:
+                    print("ok")
 
-        if humi >= 90:
-                    print 'ACHTUNG!!!! Zimmer zu feucht! Feuchtigkeit = ', humi,'%'
+        if irtemp >= 23:
+                    print("Zimmer zu warm")
         else:
-                    print 'Zimmerfeuchtigkeit OK = ',humi ,'%'
-
-        if rtemp >= 23:
-                    print 'ACHTUNG!!!! Zimmer zu warm. Temperatur = ',rtemp,'Grad'
+                    print("ok")
+        if iwtemp >= 25:
+                    print("Wasser zu warm")
         else:
-                    print 'Zimmertemperatur OK = ',rtemp, 'Grad'
-        if wtemp >= 25:
-                    print 'AUCHTUNG!!!! Wasser zu warm. Temperatur = ',wtemp,'Grad'
-        else:
-                    print 'Wassertemperatur OK = ',wtemp,'Grad'
+                    print("ok")
 
 except KeyboardInterrupt:
-        print("nix")
+        #print("nix")
         s.close()
